52da4dc56ed58c2afe22535a815d1abb7108f307
Your Name <you@example.com>
2023-07-02 13:40:43 +0700

    update

--- js/index.js
+++ js/index.js
@@ -1,17 +1,32 @@
+
+// side bar
 const menu = document.querySelector(".menu");
 const navbar = document.querySelector(".navbar");
 
-
+//side bar toggle
 menu.onclick = () => {
     event.preventDefault();
     navbar.classList.toggle("active");
 }
 
+const search = document.querySelector(".search");
+const searchBox = document.querySelector(".search-box");
+search.onclick = () => {
+    event.preventDefault();
+    searchBox.classList.toggle("active");
+}
+
 document.addEventListener("click", function(e){
     if(!menu.contains(e.target) && !navbar.contains(e.target)){
         navbar.classList.remove("active");
-        console.log("oke")
+  
     }
+    if(!search.contains(e.target) && !searchBox.contains(e.target)){
+        searchBox.classList.remove("active");
+    }
+
+
+
 })
 
 
